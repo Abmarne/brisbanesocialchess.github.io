982cd93af81dc2e0d04b44699fae892823d3de13
John Bampton <jbampton@users.noreply.github.com>
2026-03-01 17:19:00 +1000

    Delete build.sh

--- build.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-# Check if the branch is gh-pages
-if [ "$CF_PAGES_BRANCH" == "gh-pages" ]; then
-	echo "Skipping build for gh-pages branch."
-	exit 0
-fi
-
-# Navigate to your actual worker subdirectory and build
-cd packages/cfsite || exit
